1d436ee10b30e0e21031335840c4053fe943f066
Preeti shukla <shuklapreeti438@gmail.com>
2026-07-19 01:12:24 +0530

    fix breakFast typo in meal type

--- client/src/Pages/Foodlog.tsx
+++ client/src/Pages/Foodlog.tsx
@@ -107,7 +107,7 @@ const handleImageChange = async(e:React.ChangeEvent<HTMLInputElement>)=>{
     const hour=new Date().getHours()
 
     if(hour>=0 && hour<12){
-      mealType='breakFast';
+      mealType='breakfast';
 
     }else if(hour>=12 && hour<16){
       mealType='lunch'
